b880bd7e35d424b107eeba1853c1381d5b1ca3e3
Ian Flores <23100067@ue.edu.pe>
2025-11-22 08:29:17 -0500

    Initial commit

---
+++ app/src/androidTest/java/PC2/pc022310006723100113/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package PC2.pc022310006723100113
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("PC2.pc022310006723100113", appContext.packageName)
+    }
+}
